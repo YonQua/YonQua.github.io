88d2083d90dad231cb6c18ffa48419654211bc21
leishao <leishao@outlook.com>
2023-11-07 14:52:26 +0800

    Update ss5.sh

--- ss5.sh
+++ ss5.sh
@@ -1,86 +1,49 @@
+
+
 #!/bin/bash
 
-# Function to install and start the SOCKS5 service
-install_and_start_socks() {
-    # Create Systemd service file for sockd
-    cat <<EOF > /etc/systemd/system/sockd.service
-    [Unit]
-    Description=Socks Service
-    After=network.target nss-lookup.target
+sudo apt update
+sudo apt install dante-server
 
-    [Service]
-    User=nobody
-    CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-    AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-    NoNewPrivileges=true
-    ExecStart=/usr/local/bin/socks run -config /etc/socks/config.yaml
-    Restart=on-failure
-    RestartPreventExitStatus=23
-    LimitNPROC=10000
-    LimitNOFILE=1000000
+sudo rm /etc/danted.conf
 
-    [Install]
-    WantedBy=multi-user.target
-    EOF
 
-    # Configure and start the SOCKS5 service
-    config_install() {
-        port="1080"  # Replace with the desired port number
-        user="your_username"  # Replace with your chosen username
-        passwd="your_password"  # Replace with your chosen password
 
-        # Create configuration file for socks
-        mkdir -p /etc/socks
-        cat <<CONFIG_EOF > /etc/socks/config.yaml
-        {
-            "log": {
-                "loglevel": "warning"
-            },
-            "routing": {
-                "domainStrategy": "AsIs"
-            },
-            "inbounds": [
-                {
-                    "listen": "0.0.0.0",
-                    "port": "$port",
-                    "protocol": "socks",
-                    "settings": {
-                        "auth": "password",
-                        "accounts": [
-                            {
-                                "user": "$user",
-                                "pass": "$passwd"
-                            }
-                        ],
-                        "udp": true
-                    },
-                    "streamSettings": {
-                        "network": "tcp"
-                    }
-                }
-            ],
-            "outbounds": [
-                {
-                    "protocol": "freedom",
-                    "tag": "direct"
-                },
-                {
-                    "protocol": "blackhole",
-                    "tag": "block"
-                }
-            ]
-        }
-        CONFIG_EOF
+# 创建并编辑 /etc/danted.conf
+sudo nano /etc/danted.conf <<EOF
+logoutput: syslog
+user.privileged: root
+user.unprivileged: nobody
 
-        # Start the sockd.service
-        systemctl daemon-reload
-        systemctl enable sockd.service &> /dev/null
-        systemctl start sockd.service
-    }
+# The listening network interface or address.
+internal: 0.0.0.0 port=1080
 
-    # Call the function to configure and start SOCKS5 service
-    config_install
+# The proxying network interface or address.
+external: eth0
+
+# socks-rules determine what is proxied through the external interface.
+socksmethod: username
+
+# client-rules determine who can connect to the internal interface.
+clientmethod: none
+
+client pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
 }
 
-# Run the function to install and start SOCKS5
-install_and_start_socks
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0/0
+}
+EOF
+
+
+
+sudo useradd -r -s /bin/false leishao
+echo 'leishao' | sudo passwd --stdin leishao
+
+
+
+sudo systemctl restart danted.service
+
+
+systemctl status danted.service
