e6fc8210d88933b8e53aee18879eaa54d21fc57c
leishao <leishao@outlook.com>
2023-11-08 19:13:57 +0800

    Update dante_ss5.sh

--- dante_ss5.sh
+++ dante_ss5.sh
@@ -1,52 +1,63 @@
 #!/bin/bash
 
-# 定义默认参数
-SOCKSPORT=2445 
-socksUser="leishao"
-socksPass="leishao"
+# SOCKS5 安装脚本
 
-# 安装 SOCKS5 代理服务器
+# 定义变量
+PROXY_PORT=2445
+PROXY_USER=leishao
+PROXY_PASS=leishao
+
+# 检查是否已安装
+if [ -f /etc/danted.conf ]; then
+  echo "SOCKS5 已安装,退出"
+  exit
+fi
+
+# 安装
+echo "安装 SOCKS5 ..." 
 apt-get update
-apt-get install wget nano dante-server netcat -y &> /dev/null | echo '[*] Installing SOCKS5 Server...'
-cat <<'EOF'> /etc/danted.conf
-logoutput: /var/log/socks.log
-internal: 0.0.0.0 port = SOCKSPORT
-external: SOCKSINET
-socksmethod: SOCKSAUTH
-user.privileged: root
-user.notprivileged: nobody
+apt-get install -y dante-server || { echo "安装失败"; exit 1; }
 
+# 配置
+echo "配置 SOCKS5 ..."
+
+cat > /etc/danted.conf <<EOF
+port = $PROXY_PORT 
+method = username
+user.privileged = root
+user.notprivileged = nobody
 client pass {
- from: 0.0.0.0/0 to: 0.0.0.0/0
- log: error connect disconnect
- }
- 
-client block {
- from: 0.0.0.0/0 to: 0.0.0.0/0
- log: connect error
- }
- 
+  from: 0.0.0.0/0 to: 0.0.0.0/0
+  log: error connect disconnect
+}
 socks pass {
- from: 0.0.0.0/0 to: 0.0.0.0/0
- log: error connect disconnect
- }
- 
-socks block {
- from: 0.0.0.0/0 to: 0.0.0.0/0
- log: connect error
- }
+  from: 0.0.0.0/0 to: 0.0.0.0/0
+  log: error connect disconnect
+}
 EOF
-sed -i "s/SOCKSPORT/$SOCKSPORT/g" /etc/danted.conf  
-sed -i "s/SOCKSAUTH/username/g" /etc/danted.conf
 
-useradd -m -s /bin/false $socksUser
-echo -e "$socksPass\n$socksPass\n" | passwd $socksUser
+# 创建服务脚本
+cat > /etc/systemd/system/danted.service <<EOF
+[Unit]
+Description=Dante SOCKS5 proxy server
+After=network.target
+[Service]
+ExecStart=/usr/sbin/danted --foreground
+[Install]  
+WantedBy=multi-user.target
+EOF
 
-systemctl restart danted
+# 添加用户和密码
+useradd -m -s /bin/false -p $(openssl passwd -1 $PROXY_PASS) $PROXY_USER
+
+# 启动服务
 systemctl enable danted
+systemctl restart danted
 
-# 显示信息
-echo "SOCKS5 代理已安装"
-echo "端口:$SOCKSPORT"
-echo "用户名:$socksUser"
-echo "密码:$socksPass"
+# 打开端口
+iptables -I INPUT -p tcp --dport $PROXY_PORT -j ACCEPT
+
+echo "SOCKS5 安装完成"
+echo "端口:$PROXY_PORT"
+echo "用户名:$PROXY_USER"
+echo "密码: $PROXY_PASS"
