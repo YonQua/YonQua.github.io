af34936ed0587936820d9df13dc577c98ce4720f
leishao <leishao@outlook.com>
2024-01-21 14:50:10 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-DEFAULT_START_PORT=20000                         # Default starting port
-DEFAULT_SOCKS_USERNAME="userb"                   # Default SOCKS username
-DEFAULT_SOCKS_PASSWORD="passwordb"               # Default SOCKS password
+DEFAULT_START_PORT=20000                         #默认起始端口
+DEFAULT_SOCKS_USERNAME="userb"                   #默认socks账号
+DEFAULT_SOCKS_PASSWORD="passwordb"               #默认socks密码
 
 IP_ADDRESSES=($(hostname -I))
 
 install_xray() {
-    echo "安装 Xray..."
-    apt update
-    apt-get install unzip -y
-
-    if [ -x "$(command -v xray)" ]; then
-        echo "Xray is already installed, skipping installation."
-    else
-        wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-        unzip -o Xray-linux-64.zip
-        mv xray /usr/local/bin/xrayL
-        chmod +x /usr/local/bin/xrayL
-
-        cat <<EOF >/etc/systemd/system/xrayL.service
+	echo "安装 Xray..."
+	apt-get install unzip -y
+	wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+	unzip -o Xray-linux-64.zip
+	mv xray /usr/local/bin/xrayL
+	chmod +x /usr/local/bin/xrayL
+	cat <<EOF >/etc/systemd/system/xrayL.service
 [Unit]
 Description=XrayL Service
 After=network.target
 
 [Service]
-ExecStart=/usr/local/bin/xray -c /etc/xrayL/config.toml
+ExecStart=/usr/local/bin/xrayL -c /etc/xrayL/config.toml
 Restart=on-failure
 User=nobody
 RestartSec=3
@@ -33,40 +27,34 @@ RestartSec=3
 [Install]
 WantedBy=multi-user.target
 EOF
-
-        systemctl daemon-reload
-        systemctl enable xrayL.service
-        systemctl start xrayL.service
-        echo "xrayL 安装完成."
-    fi
+	systemctl daemon-reload
+	systemctl enable xrayL.service
+	systemctl start xrayL.service
+	echo "Xray 安装完成."
 }
-
 config_xray() {
-    config_type="socks"
-    read -p "Starting port (default $DEFAULT_START_PORT): " START_PORT
-    START_PORT=${START_PORT:-$DEFAULT_START_PORT}
+	config_type=$1
+	mkdir -p /etc/xrayL
+	read -p "起始端口 (默认 $DEFAULT_START_PORT): " START_PORT
+	START_PORT=${START_PORT:-$DEFAULT_START_PORT}
+	read -p "SOCKS 账号 (默认 $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
+	SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
+	read -p "SOCKS 密码 (默认 $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
+	SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
 
-    read -p "SOCKS username (default $DEFAULT_SOCKS_USERNAME): " SOCKS_USERNAME
-    SOCKS_USERNAME=${SOCKS_USERNAME:-$DEFAULT_SOCKS_USERNAME}
-
-    read -p "SOCKS password (default $DEFAULT_SOCKS_PASSWORD): " SOCKS_PASSWORD
-    SOCKS_PASSWORD=${SOCKS_PASSWORD:-$DEFAULT_SOCKS_PASSWORD}
-
-    for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
+	for ((i = 0; i < ${#IP_ADDRESSES[@]}; i++)); do
 		config_content+="[[inbounds]]\n"
 		config_content+="port = $((START_PORT + i))\n"
 		config_content+="protocol = \"$config_type\"\n"
 		config_content+="tag = \"tag_$((i + 1))\"\n"
 		config_content+="[inbounds.settings]\n"
-		
-        config_content+="auth = \"password\"\n"
+		config_content+="auth = \"password\"\n"
 		config_content+="udp = true\n"
 		config_content+="ip = \"${IP_ADDRESSES[i]}\"\n"
 		config_content+="[[inbounds.settings.accounts]]\n"
 		config_content+="user = \"$SOCKS_USERNAME\"\n"
 		config_content+="pass = \"$SOCKS_PASSWORD\"\n"
-		
-        config_content+="[[outbounds]]\n"
+		config_content+="[[outbounds]]\n"
 		config_content+="sendThrough = \"${IP_ADDRESSES[i]}\"\n"
 		config_content+="protocol = \"freedom\"\n"
 		config_content+="tag = \"tag_$((i + 1))\"\n\n"
@@ -74,18 +62,17 @@ config_xray() {
 		config_content+="type = \"field\"\n"
 		config_content+="inboundTag = \"tag_$((i + 1))\"\n"
 		config_content+="outboundTag = \"tag_$((i + 1))\"\n\n\n"
-
-    done
-    echo -e "$config_content" >> /etc/xrayL/config.toml
-    systemctl restart xrayL.service
-    systemctl --no-pager status xrayL.service
-    echo ""
-    echo "$config_type configuration generated successfully"
-    echo "Starting port: $START_PORT"
-    echo "Ending port: $((START_PORT + ${#IP_ADDRESSES[@]} - 1))"
-    echo "SOCKS username: $SOCKS_USERNAME"
-    echo "SOCKS password: $SOCKS_PASSWORD"
-    echo ""
+	done
+	echo -e "$config_content" >/etc/xrayL/config.toml
+	systemctl restart xrayL.service
+	systemctl --no-pager status xrayL.service
+	echo ""
+	echo "生成 $config_type 配置完成"
+	echo "起始端口:$START_PORT"
+	echo "结束端口:$(($START_PORT + $i - 1))"
+	echo "socks账号:$SOCKS_USERNAME"
+	echo "socks密码:$SOCKS_PASSWORD"
+	echo ""
 }
 main() {
     [ -x "$(command -v xray)" ] || install_xray
@@ -93,7 +80,5 @@ main() {
 }
 
 main
-
-
 
 ## bash <(curl -fsSLk https://raw.githubusercontent.com/YonQua/YonQua.github.io/main/socks5.sh)
