0b13a3f3cd732a0a74c2d8a3de793d58ae75ad79
leishao <leishao@outlook.com>
2024-03-06 17:28:55 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -21,7 +21,9 @@ sudo apt-get install -y \
 
 # 添加Docker的官方GPG密钥
 echo "添加Docker的官方GPG密钥..."
-echo "y" | curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo rm -f /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 
 # 设置稳定版的仓库
 echo "设置Docker的稳定版仓库..."
